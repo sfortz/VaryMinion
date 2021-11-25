12c2473b67a4a23ee39986ecc2610d68c1a43f94
Sophie <sophie.fortz@unamur.be>
2021-11-25 10:16:18 +0100

    Claroline scripts

---
+++ scripts/training_NN/Claroline-dis_10-tensorflow.py
@@ -0,0 +1,27 @@
+import training_Model_tensorflow as minion
+
+if __name__ == "__main__":
+
+    for i in range(0, 10):
+        print("Exécution " + str(i) + " : ")
+        minion.main("claroline-dis_10.csv", "GRU", True, 20, 30, 128, 0.66)
+
+    for i in range(0, 10):
+        print("Exécution " + str(i) + " : ")
+        minion.main("claroline-dis_10.csv", "LSTM", True, 20, 30, 128, 0.66)
+
+    for i in range(0, 10):
+        print("Exécution " + str(i) + " : ")
+        minion.main("claroline-dis_10.csv", "GRU", True, 20, 10, 128, 0.66)
+
+    for i in range(0, 10):
+        print("Exécution " + str(i) + " : ")
+        minion.main("claroline-dis_10.csv", "LSTM", True, 20, 10, 128, 0.66)
+
+    for i in range(0, 10):
+        print("Exécution " + str(i) + " : ")
+        minion.main("claroline-dis_10.csv", "GRU", True, 20, 5, 128, 0.66)
+
+    for i in range(0, 10):
+        print("Exécution " + str(i) + " : ")
+        minion.main("claroline-dis_10.csv", "LSTM", True, 20, 5, 128, 0.66)

a522e72d790aa03df427b26e81518fbc3b94ea58
Sophie <sophie.fortz@unamur.be>
2021-11-25 09:21:21 +0100

    Update Claroline-tensorflow.py

--- scripts/training_NN/Claroline-tensorflow.py
+++ scripts/training_NN/Claroline-tensorflow.py
@@ -4,30 +4,6 @@ if __name__ == "__main__":
 
     for i in range(0, 10):
         print("Exécution " + str(i) + " : ")
-        minion.main("claroline-dis_50.csv", "GRU", True, 20, 30, 128, 0.66)
-
-    for i in range(0, 10):
-        print("Exécution " + str(i) + " : ")
-        minion.main("claroline-dis_50.csv", "LSTM", True, 20, 30, 128, 0.66)
-
-    for i in range(0, 10):
-        print("Exécution " + str(i) + " : ")
-        minion.main("claroline-dis_50.csv", "GRU", True, 20, 10, 128, 0.66)
-
-    for i in range(0, 10):
-        print("Exécution " + str(i) + " : ")
-        minion.main("claroline-dis_50.csv", "LSTM", True, 20, 10, 128, 0.66)
-
-    for i in range(0, 10):
-        print("Exécution " + str(i) + " : ")
-        minion.main("claroline-dis_50.csv", "GRU", True, 20, 5, 128, 0.66)
-
-    for i in range(0, 10):
-        print("Exécution " + str(i) + " : ")
-        minion.main("claroline-dis_50.csv", "LSTM", True, 20, 5, 128, 0.66)
-
-    for i in range(0, 10):
-        print("Exécution " + str(i) + " : ")
         minion.main("claroline-dis_10.csv", "GRU", True, 20, 30, 128, 0.66)
 
     for i in range(0, 10):
@@ -49,3 +25,29 @@ if __name__ == "__main__":
     for i in range(0, 10):
         print("Exécution " + str(i) + " : ")
         minion.main("claroline-dis_10.csv", "LSTM", True, 20, 5, 128, 0.66)
+
+
+
+    # for i in range(0, 10):
+    #     print("Exécution " + str(i) + " : ")
+    #     minion.main("claroline-dis_50.csv", "GRU", True, 20, 30, 128, 0.66)
+    #
+    # for i in range(0, 10):
+    #     print("Exécution " + str(i) + " : ")
+    #     minion.main("claroline-dis_50.csv", "LSTM", True, 20, 30, 128, 0.66)
+    #
+    # for i in range(0, 10):
+    #     print("Exécution " + str(i) + " : ")
+    #     minion.main("claroline-dis_50.csv", "GRU", True, 20, 10, 128, 0.66)
+
+    # for i in range(0, 10):
+    #     print("Exécution " + str(i) + " : ")
+    #     minion.main("claroline-dis_50.csv", "LSTM", True, 20, 10, 128, 0.66)
+    #
+    # for i in range(0, 10):
+    #     print("Exécution " + str(i) + " : ")
+    #     minion.main("claroline-dis_50.csv", "GRU", True, 20, 5, 128, 0.66)
+    #
+    # for i in range(0, 10):
+    #     print("Exécution " + str(i) + " : ")
+    #     minion.main("claroline-dis_50.csv", "LSTM", True, 20, 5, 128, 0.66)

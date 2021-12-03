1b4432ec1beba044512b7b0c279c4bbaf7150ef1
Sophie <sophie.fortz@unamur.be>
2021-12-03 14:35:35 +0100

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Initialisation script for Dragon2
+
+# ------------------------- work -------------------------
+
+module load foss
+module load releases/2019b
+module load TensorFlow/2.3.1-fosscuda-2019b-Python-3.7.4
+module load scikit-learn/0.22.1-fosscuda-2019b-Python-3.7.4

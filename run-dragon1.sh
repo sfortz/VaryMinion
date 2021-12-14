259b7e45761077ac03a7a75d8fa9465987b9c5d3
Sophie <sophie.fortz@unamur.be>
2021-12-14 22:20:21 +0100

    Update run-dragon1.sh

--- run-dragon1.sh
+++ run-dragon1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Submission script for Dragon2
+# Submission script for Dragon1
 #SBATCH --job-name=VaryMinions
 #SBATCH --time=3-01:00:00 # days-hh:mm:ss
 #
@@ -9,7 +9,7 @@
 #SBATCH --mem-per-cpu=12288 # 12GB
 #SBATCH --partition=gpu
 #
-#SBATCH --array=1-40
+#SBATCH --array=3,4,7-40
 #
 #SBATCH --mail-user=sophie.fortz@unamur.be
 #SBATCH --mail-type=ALL
@@ -21,5 +21,5 @@ module load releases/2019b
 module load TensorFlow/2.4.1-fosscuda-2019b-Python-3.7.4
 echo "Job start at $(date)"
 cd scripts/training_NN/
-python3 DS1-10unit.py
+python3 job-array-BPIC.py
 echo "Job end at $(date)"

cae1f90f0a212e791decf86e53e65bb21c5c2436
Sophie <sophie.fortz@unamur.be>
2021-12-14 13:15:14 +0100

    Update script array

--- run-dragon1.sh
+++ run-dragon1.sh
@@ -9,6 +9,8 @@
 #SBATCH --mem-per-cpu=12288 # 12GB
 #SBATCH --partition=gpu
 #
+#SBATCH --array=1-40
+#
 #SBATCH --mail-user=sophie.fortz@unamur.be
 #SBATCH --mail-type=ALL
 #
@@ -19,5 +21,5 @@ module load releases/2019b
 module load TensorFlow/2.4.1-fosscuda-2019b-Python-3.7.4
 echo "Job start at $(date)"
 cd scripts/training_NN/
-python3 Claroline-dis_50-tensorflow.py
+python3 DS1-10unit.py
 echo "Job end at $(date)"
--- run-dragon2.sh
+++ run-dragon2.sh
@@ -9,7 +9,7 @@
 #SBATCH --mem-per-cpu=12288 # 12GB
 #SBATCH --partition=gpu
 #
-#SBATCH --array=111-115
+#SBATCH --array=1-20
 #
 #SBATCH --mail-user=sophie.fortz@unamur.be
 #SBATCH --mail-type=ALL
@@ -22,5 +22,5 @@ module load TensorFlow/2.3.1-fosscuda-2019b-Python-3.7.4
 module load scikit-learn/0.22.1-fosscuda-2019b-Python-3.7.4
 echo "Job start at $(date)"
 cd scripts/training_NN/
-python3 job-array-definition.py
+python3 job-array-claroline-10-dis.py
 echo "Job end at $(date)"

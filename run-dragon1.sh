9b4177f66eb7d1fe15d557011a1053e4d6e41850
Sophie <sophie.fortz@unamur.be>
2021-12-17 08:16:28 +0100

    local scratch managing

--- run-dragon1.sh
+++ run-dragon1.sh
@@ -4,12 +4,14 @@
 #SBATCH --time=3-01:00:00 # days-hh:mm:ss
 #
 #SBATCH --ntasks=1
-#SBATCH --gres="gpu:1"
+# For the K20M
+#SBATCH --ntasks-per-core=1
+#SBATCH --gres=gpu:kepler:1
+#SBATCH --partition=gpu
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=12288 # 12GB
-#SBATCH --partition=gpu
 #
-#SBATCH --array=1-40
+#SBATCH --array=2
 #
 #SBATCH --mail-user=sophie.fortz@unamur.be
 #SBATCH --mail-type=ALL
@@ -19,7 +21,12 @@
 module load foss
 module load releases/2019b
 module load TensorFlow/2.4.1-fosscuda-2019b-Python-3.7.4
+mkdir -p $LOCALSCRATCH/$SLURM_JOB_ID
+rsync -azu $CECIHOME/VaryMinions $LOCALSCRATCH/$SLURM_JOB_ID/
+cd $LOCALSCRATCH/$SLURM_JOB_ID/VaryMinions/scripts/training_NN/
 echo "Job start at $(date)"
-cd scripts/training_NN/
-python3 job-array-BPIC.py
+python3 Claroline-dis_10.py
 echo "Job end at $(date)"
+rsync -azu $LOCALSCRATCH/$SLURM_JOB_ID/VaryMinions/ $CECIHOME/VaryMinions/
+rm -rf $LOCALSCRATCH/$SLURM_JOB_ID
+
--- run-dragon2.sh
+++ run-dragon2.sh
@@ -9,7 +9,7 @@
 #SBATCH --mem-per-cpu=12288 # 12GB
 #SBATCH --partition=gpu
 #
-#SBATCH --array=1-20
+#SBATCH --array=14-20
 #
 #SBATCH --mail-user=sophie.fortz@unamur.be
 #SBATCH --mail-type=ALL
@@ -20,7 +20,12 @@ module load foss
 module load releases/2019b
 module load TensorFlow/2.3.1-fosscuda-2019b-Python-3.7.4
 module load scikit-learn/0.22.1-fosscuda-2019b-Python-3.7.4
+mkdir -p $LOCALSCRATCH/$SLURM_JOB_ID
+rsync -azu $CECIHOME/VaryMinions $LOCALSCRATCH/$SLURM_JOB_ID/
+cd $LOCALSCRATCH/$SLURM_JOB_ID/VaryMinions/scripts/training_NN/
 echo "Job start at $(date)"
-cd scripts/training_NN/
 python3 job-array-claroline-10-dis.py
 echo "Job end at $(date)"
+rsync -azu $LOCALSCRATCH/$SLURM_JOB_ID/VaryMinions/ $CECIHOME/VaryMinions/
+rm -rf $LOCALSCRATCH/$SLURM_JOB_ID
+

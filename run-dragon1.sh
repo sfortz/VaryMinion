281471f378c3cbefe559d92a0f7a7d0b2607902a
Sophie <sophie.fortz@unamur.be>
2022-11-14 13:51:07 +0100

    Update run-dragon1.sh

--- run-dragon1.sh
+++ run-dragon1.sh
@@ -9,9 +9,9 @@
 #SBATCH --gres=gpu:kepler:1
 #SBATCH --partition=gpu
 #SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=12288 # 12GB
+#SBATCH --mem-per-cpu=4000 # 4GB
 #
-#SBATCH --array=3-5
+#SBATCH --array=6-10
 #SBATCH --output="slurm-output/slurm-%A_%a.out"
 #
 #SBATCH --mail-user=sophie.fortz@unamur.be
@@ -19,26 +19,25 @@
 #
 # ------------------------- work -------------------------
 
-cd
-source ceci-venv/bin/activate
-cd $CECIHOME/VaryMinions
-
-echo "ceci-venv activated"
-
 module load releases/2020b
+module load Python/3.8.6-GCCcore-10.2.0
 module load foss/2020b
 module load cuDNN/8.0.4.30-CUDA-11.1.1
 module load CUDA/11.1.1-GCC-10.2.0
-
 echo "module loaded"
+
+cd
+source ceci-venv/bin/activate
+echo "ceci-venv activated"
+cd $CECIHOME/VaryMinions
+
 
 mkdir -p $LOCALSCRATCH/$SLURM_JOB_ID
 rsync -azu $CECIHOME/VaryMinions $LOCALSCRATCH/$SLURM_JOB_ID/
 cd $LOCALSCRATCH/$SLURM_JOB_ID/VaryMinions/scripts/training_NN/
 echo "Job start at $(date)"
-srun python3 job-array-claroline-10-rand.py
+srun python3 job-array-claroline-10-dis.py
 rsync -azu $LOCALSCRATCH/$SLURM_JOB_ID/VaryMinions/results/training_metrics/ $CECIHOME/VaryMinions/results/training_metrics/
 echo "Job end at $(date)"
 rm -rf $LOCALSCRATCH/$SLURM_JOB_ID
 deactivate
-

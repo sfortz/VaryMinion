0e1d0b4445f8ffff6231d23d5459636dc37a6079
Sophie <sophie.fortz@unamur.be>
2022-11-18 14:32:11 +0100

    Update run-dragon1.sh

--- run-dragon1.sh
+++ run-dragon1.sh
@@ -11,7 +11,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=4000 # 4GB
 #
-#SBATCH --array=15-20
+#SBATCH --array=1-5
 #SBATCH --output="slurm-output/slurm-%A_%a.out"
 #
 #SBATCH --mail-user=sophie.fortz@unamur.be
@@ -35,7 +35,7 @@ mkdir -p $LOCALSCRATCH/$SLURM_JOB_ID
 rsync -azu $CECIHOME/VaryMinions $LOCALSCRATCH/$SLURM_JOB_ID/
 cd $LOCALSCRATCH/$SLURM_JOB_ID/VaryMinions/scripts/training_NN/
 echo "Job start at $(date)"
-srun python3 job-array-claroline-10-dis.py
+srun python3 job-array-claroline-50-rand.py
 rsync -azu $LOCALSCRATCH/$SLURM_JOB_ID/VaryMinions/results/training_metrics/ $CECIHOME/VaryMinions/results/training_metrics/
 echo "Job end at $(date)"
 rm -rf $LOCALSCRATCH/$SLURM_JOB_ID

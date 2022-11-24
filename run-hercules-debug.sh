66b72c7ae568d3ada8df9532a468edb67e85e4a4
Sophie <sophie.fortz@unamur.be>
2022-11-24 09:49:27 +0100

    Update run-hercules-debug.sh

--- run-hercules-debug.sh
+++ run-hercules-debug.sh
@@ -9,7 +9,7 @@
 #SBATCH --mem-per-cpu=4000 # 4GB
 #SBATCH --partition=gpu-debug
 #
-#SBATCH --array=6-10
+#SBATCH --array=11-15
 #SBATCH --output="slurm-output/slurm-%A_%a.out"
 #
 #SBATCH --mail-user=sophie.fortz@unamur.be

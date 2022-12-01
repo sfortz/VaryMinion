372dd647352cacad380c57edf63f06c4444aab71
Sophie <sophie.fortz@unamur.be>
2022-12-01 17:27:33 +0100

    Delete debug.py

--- scripts/training_NN/debug.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import tensorflow as tf
-import sklearn
-
-if __name__ == "__main__":
-    print("Numpy version is ", np.__version__)
-    print("Tensorflow version is ", tf.__version__)
-    print("Scikit-Learn version is ", sklearn.__version__)

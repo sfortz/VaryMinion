b14b988cc313a34bc664aa74334f3609028cf547
Sophie <sophie.fortz@unamur.be>
2022-12-02 16:43:13 +0100

    Update run-dragon1.sh

--- run-dragon1.sh
+++ run-dragon1.sh
@@ -11,7 +11,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=4000 # 4GB
 #
-#SBATCH --array=8,12,13
+#SBATCH --array=15,18,17
 #SBATCH --output="slurm-output/slurm-%A_%a.out"
 #
 #SBATCH --mail-user=sophie.fortz@unamur.be

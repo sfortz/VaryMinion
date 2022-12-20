119c46ff19233ac69fb92b4d4d52c4a9efb6f3d7
Sophie <sophie.fortz@unamur.be>
2022-12-20 18:10:40 +0100

    Extracting CSV reader utilities

--- scripts/analyzes/get_total_time.py
+++ scripts/analyzes/get_total_time.py
@@ -1,60 +1,7 @@
 import os
-import re
 import pandas as pd
 from datetime import datetime, timedelta
-
-
-def parse_match(match, regex):
-    if match:
-        x = match.group()
-        l0 = re.sub(r'^' + re.escape(regex), '', x)
-        s = re.sub(re.escape('_') + r'$', '', l0)
-    else:
-        print("BUG: " + str(match) + regex)
-        s = ''
-    return s
-
-
-def parse_file_name(file_name):
-    print("Name : " + file_name)
-    x = re.search(".*.csv", file_name).group()
-    dataset = re.sub(re.escape('.csv') + r'$', '', x)
-
-    x = re.search("metrics_[A-Z]*_", file_name)
-    model = parse_match(x, 'metrics_')
-
-    x = re.search("nb_epochs_[0-9]*_", file_name)
-    nb_epoch = parse_match(x, 'nb_epochs_')
-
-    x = re.search("nb_unit_[0-9]*_", file_name)
-    nb_unit = parse_match(x, 'nb_unit_')
-
-    x = re.search("batch_size_[0-9]*_", file_name)
-    batch_size = parse_match(x, 'batch_size_')
-
-    x = re.search("training_set_size_[0-9]*.[0-9]*_", file_name)
-    training_set_size = parse_match(x, 'training_set_size_')
-
-    x = re.search("_CPU|_GPU", file_name)
-    proc = parse_match(x, '_')
-
-    x = re.search("_tensorflow|_plaid_ml", file_name)
-    backend = parse_match(x, '_')
-
-    x = re.search("_[a-zA-Z]*$", file_name)
-    layer = parse_match(x, '_')
-
-    x = re.search(backend + "_.*_" + layer, file_name)
-    if re.search("_sigmoid_", x.group()):
-        x = parse_match(x, backend + '_')
-        loss = re.sub("_sigmoid_" + layer, '', x)
-        activation = "sigmoid"
-    else:
-        x = parse_match(x, backend + '_')
-        loss = re.sub("_tanh_" + layer, '', x)
-        activation = "tanh"
-
-    return dataset, model, nb_epoch, nb_unit, batch_size, training_set_size, proc, backend, layer, loss, activation
+from csv_reader import parse_file_name
 
 
 def get_time(data):
